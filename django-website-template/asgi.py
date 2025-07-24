f4eaee1310b7c6ca2ca40b3d8f981e63c8e7fa87
Igor <104066443+OldXrist@users.noreply.github.com>
2025-07-24 16:16:37 +0300

    Name refactor

--- django-website-template/asgi.py
+++ django-website-template/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_website_template project.
+ASGI config for django-website-template project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- django-website-template/wsgi.py
+++ django-website-template/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django_website_template project.
+WSGI config for django-website-template project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
